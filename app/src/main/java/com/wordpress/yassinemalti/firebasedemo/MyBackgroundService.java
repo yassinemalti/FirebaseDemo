d7c4a7060a19753ec07f55e8010d5f9827ea7fd9
Yassine MALTI <yassinemalti@gmail.com>
2017-03-21 19:07:57 +0100

    210320171907

--- app/src/main/java/com/wordpress/yassinemalti/firebasedemo/MyBackgroundService.java
+++ app/src/main/java/com/wordpress/yassinemalti/firebasedemo/MyBackgroundService.java
@@ -57,8 +57,6 @@ public class MyBackgroundService extends Service {
                 myRef.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        // This method is called once with the initial value and again
-                        // whenever data at this location is updated.
                         String value = dataSnapshot.getValue(String.class);
                         sendNotification(value);
                     }
